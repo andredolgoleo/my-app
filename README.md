eb2e9b675f1030121cbe3022edabbad4452237ef
Andrey <andrej.dolgov.042110@gmail.com>
2022-09-30 19:29:47 +0300

    added api requests

--- README.md
+++
@@ -1,7 +0,0 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-Hi every one! This my weather-informer react-app. I would be glad if you use it
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getCountryCoords } from './api/api'
+import { Header } from './components/Header'
 
-function App() {
+export const App: React.FC = () => {
+  const [currentCountry, setCurrentCountry] = useState(null);
+
+  async function loadData() {
+    const a = await getCountryCoords('kropyvnytskyi');
+
+    setCurrentCountry(a[0]);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [])
+
   return (
-    <div className="App">
-      
-    </div>
+    currentCountry && <Header currentCountry={currentCountry} />
   );
 }
-
-export default App;
